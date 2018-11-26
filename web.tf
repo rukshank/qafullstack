2aab510c7bb0cefa7bed71a61980d94d66805ce4
Jenkins <jenkins@ip-10-23-1-122.us-east-2.compute.internal>
2018-11-26 11:21:17 +0000

    Adding new web.tf

---
+++ web.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+ region = "us-east-2"
+}
+
+resource "aws_instance" "rukshankweb" {
+  ami = "ami-0782e9ee97725263d"
+  instance_type = "t2.micro"
+  key_name = "fullstackkey"
+  subnet_id = "subnet-04adf818d05d0928f"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["sg-04c7ac1c35820a26c"]
+
+ tags {
+ Name = "rukshankweb"
+}
+
+}
